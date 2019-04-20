04994c2c933e5ce9029ae641560d247a69a12fdc
nevroje <joakim.berglind@gmail.com>
2019-04-21 00:04:49 +0200

    added function to app.js and events to buttons for parent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import ResultComponent from "./components/ResultComponent";
-import KeyPadComponent from "./components/KeyPadComponent";
+import ResultComponent from "../src/components/ResultComponent";
+import KeyPadComponent from "../src/components/KeypadComponent";
 
 class App extends Component {
   constructor(props) {
@@ -10,11 +10,49 @@ class App extends Component {
     };
   }
 
+  onClick = button => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace;
+    } else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  };
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="calculator-body">
-          <h1>Simple Calculator</h1>
+          <h1>Calculator</h1>
           <ResultComponent result={this.state.result} />
           <KeyPadComponent onClick={this.onClick} />
         </div>
